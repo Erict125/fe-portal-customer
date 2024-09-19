04598deaba70721fd9be72ec62c2a3e220af04c9
Erict125 <libravainglory@gmail.com>
2024-09-19 11:16:08 +0700

    router
    render router

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,44 @@
-dicoba
+//import vue router
+import { createRouter, createWebHistory } from 'vue-router'
+
+//define a routes
+const routes = [
+    {
+        path: '/blog',
+        name: 'blog',
+        component: () => import( /* webpackChunkName: "home" */ '../components/Blog.vue')
+    },
+    // {
+    //     path: '/',
+    //     name: 'home',
+    //     component: () => import( /* webpackChunkName: "home" */ '../components/Hero.vue')
+    // },
+    // {
+    //     path: '/contact',
+    //     name: 'contact',
+    //     component: () => import( /* webpackChunkName: "home" */ '../components/Contact.vue')
+    // },
+    // {
+    //     path: '/posts',
+    //     name: 'posts.index',
+    //     component: () => import( /* webpackChunkName: "index" */ '../views/posts/index.vue')
+    // },
+    // {
+    //     path: '/create',
+    //     name: 'posts.create',
+    //     component: () => import( /* webpackChunkName: "create" */ '../views/posts/create.vue')
+    // },
+    // {
+    //     path: '/edit/:id',
+    //     name: 'posts.edit',
+    //     component: () => import( /* webpackChunkName: "edit" */ '../views/posts/edit.vue')
+    // }
+]
+
+//create router
+const router = createRouter({
+    history: createWebHistory(),
+    routes // <-- routes,
+})
+
+export default router
